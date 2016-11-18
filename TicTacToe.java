d3c70b9cbdc79e7c27c7b4ac35081aaa6c3b94f1
freakfallout <freakfallout@gmail.com>
2016-11-17 18:15:24 -0700

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,21 @@
 package groupProject;
 
-public class TicTacToe {
-
+public class TicTacToe 
+{
+  char[] board = new char(9);
+  
+  void showBoard()
+  {
+  }
+  
+  void determineWinner()
+  {
+  }
+  
+  void chooseSpace()
+  {
+  }
+  
+  
+  
 }
